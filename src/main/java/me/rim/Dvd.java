c9fb7cabc461c80370a1304956fef116e214a99c
Rimantas Kazlauskas <konulv@gmail.com>
2022-08-22 07:53:19 +0100

    adding dvd

---
+++ src/main/java/me/rim/Dvd.java
@@ -0,0 +1,13 @@
+package me.rim;
+
+import java.time.LocalDate;
+
+public class Dvd{
+    private String title;
+    private LocalDate releaseDate;
+    private String mpaa;
+    private String director;
+    private String studio;
+    private String userRating;
+
+}
