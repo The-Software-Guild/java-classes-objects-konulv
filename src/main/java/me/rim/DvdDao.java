f4214dcc946a6b57cd9ff512bcd8913e3458ce08
Rimantas Kazlauskas <konulv@gmail.com>
2022-08-24 07:25:38 +0100

    added some other blank class files to be used in the future

---
+++ src/main/java/me/rim/DvdDao.java
@@ -0,0 +1,15 @@
+package me.rim;
+
+
+// feel like i should use a hashset for this, could also just arraylist
+// pretty sure array list index recall is o(1)
+// with some sketchy code i could give it a psuedo "id"
+public class DvdDao {
+    /*used for:
+    storing
+    reading
+    writing
+    */
+
+    
+}
---
+++ src/main/java/me/rim/View.java
@@ -0,0 +1,5 @@
+package me.rim;
+
+public class View {
+    
+}
