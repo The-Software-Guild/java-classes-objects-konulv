4cbafcd366628a834d23243df62fc8f9416fa695
Rimantas Kazlauskas <konulv@gmail.com>
2022-08-24 07:20:17 +0100

    added a template code for dvd controller

---
+++ src/main/java/me/rim/DvdController.java
@@ -0,0 +1,37 @@
+package me.rim;
+
+public class DvdController {
+
+    public void addDvd(Dvd dvd){
+        System.out.println("todo add Dvd");
+    }
+
+    public void removeDvd(Dvd dvd){
+        System.out.println("todo remove Dvd");
+    }
+
+    public void editDvd(Dvd oldDvd, Dvd newDvd){
+        System.out.println("todo edit Dvd");
+    }
+
+    public void listCollection(){
+        System.out.println("todo list collection");
+    }
+
+    public void displayDvd(){
+        System.out.println("todo display dvd info");
+    }
+
+    public void searchDvd(){
+        System.out.println("todo search for dvd");
+    }
+
+    public void loadFile(String path){
+        System.out.println("todo load file");
+    }
+
+    public void saveFile(String path){
+        System.out.println("todo save to a file");
+    }
+
+}
